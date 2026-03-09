b0a02419ec4138ac6c6e51cea924c362962c63cc
tsujisalju <purrnama@proton.me>
2026-03-10 02:34:20 +0800

    feat: landing page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,65 +1,103 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { ArrowRightIcon } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex flex-col justify-center items-center">
+      <div className="relative w-full grid place-items-center h-screen bg-chart-1">
+        <div className="noise-overlay"></div>
+        <div className="bg-background max-w-6xl w-full grid grid-cols-1 md:grid-cols-2 justify-center items-center space-y-4 rounded-lg">
+          <div className="w-full h-full m-0"></div>
+          <div className="flex flex-col space-y-8 p-16">
+            <h1 className="text-5xl font-bold">Your extracurricular achievements, verified on-chain</h1>
+            <div className="flex flex-wrap items-center gap-4 md:flex-row">
+              <Button>Connect Wallet</Button>
+              <Button>Verify a Credential</Button>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      </div>
+      <div className="w-full flex flex-col justify-center items-center space-y-8 py-32">
+        <h2 className="text-3xl font-bold">Who is BECP for?</h2>
+        <div className="w-full max-w-6xl grid grid-cols-2 grid-rows-2 place-items-center gap-4">
+          <div className="w-full">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-xl">Students</CardTitle>
+              <CardDescription>Set your career goals, find and join extracurricular events and receive micro-credentials tied to your skill portfolio.</CardDescription>
+            </CardHeader>
+            <CardFooter><Button>Start your journey</Button></CardFooter>
+            </Card>
+          </div>
+          <div className="w-full">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-xl">Event Organizers</CardTitle>
+              <CardDescription>Reward your event participants with on-chain credentials, get approved by universities to build reputation and drive engagement.</CardDescription>
+            </CardHeader>
+            <CardFooter><Button>Register your event</Button></CardFooter>
+            </Card>
+          </div>
+          <div className="w-full">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-xl">University Administrators</CardTitle>
+              <CardDescription>Manage students from your institution, control who can issue credentials to align with university development objectives.</CardDescription>
+            </CardHeader>
+            <CardFooter><Button>Connect as institution</Button></CardFooter>
+          </Card>
+          </div>
+          <div className="w-full">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-xl">HR Recruiters</CardTitle>
+              <CardDescription>Instantly verify your candidates&apos; credentials and skillsets using direct QR codes to on-chain proof.</CardDescription>
+            </CardHeader>
+            <CardFooter><Button>Verify a credential</Button></CardFooter>
+          </Card>
+          </div>
         </div>
-      </main>
+      </div>
+      <div className="w-full flex flex-col justify-center items-center space-y-8 py-32">
+        <h2 className="text-3xl font-bold">How it works</h2>
+        <div className="w-full flex flex-row justify-center items-center space-x-4">
+        <div className="w-full max-w-sm">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-xl">Issue</CardTitle>
+              <CardDescription>Explore and join extracurricular events relevant to your career goals. Receive micro-credentials in your wallet upon completion.</CardDescription>
+            </CardHeader>
+          </Card>
+          </div>
+          <div className="rounded-full border p-1">
+            <ArrowRightIcon />
+          </div>
+        <div className="w-full max-w-sm">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-xl">Own</CardTitle>
+              <CardDescription>Micro-credentials are exclusively yours and soulbound to your wallet. They contain skill metadata that add to your overall personal development.</CardDescription>
+            </CardHeader>
+            </Card>
+            <div>
+            </div>
+          </div>
+          <div className="rounded-full border p-1">
+            <ArrowRightIcon />
+          </div>
+          <div className="w-full max-w-sm">
+            <Card>
+              <CardHeader>
+                <CardTitle className="text-xl">Verify</CardTitle>
+                <CardDescription>Turn your micro-credentials into a resume you can submit for job application. Recruites can easily verify your skills with direct QR code links to on-chain proof.</CardDescription>
+              </CardHeader>
+              </Card>
+              <div>
+              </div>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
