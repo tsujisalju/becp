d9cf8aa1dad5fa1f99592cff9898a3380d6ac4c7
tsujisalju <purrnama@proton.me>
2026-03-18 02:56:13 +0800

    chore: setup analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@
 // Program Name     : frontend/app/layout.tsx
 // Description      : Layout component for entire route hierarchy. Applies fonts and metadata.
 // First Written on : Saturday, 7-Mar-2026
-// Last Modified on : Tuesday, 17-Mar-2026
+// Last Modified on : Wednesday, 18-Mar-2026
 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Roboto_Slab } from "next/font/google";
@@ -10,6 +10,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import "@rainbow-me/rainbowkit/styles.css";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics";
 
 const robotoSlab = Roboto_Slab({
   subsets: ["latin"],
@@ -41,6 +42,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
